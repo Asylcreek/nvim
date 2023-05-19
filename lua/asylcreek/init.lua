6c028335873c79998e40cf03000db2fc2a19b8e2
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-05-19 13:52:32 +0100

    Set global variables

--- lua/asylcreek/init.lua
+++ lua/asylcreek/init.lua
@@ -1,2 +1,10 @@
 require("asylcreek.remap")
 require("asylcreek.set")
+
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+vim.cmd([[ let g:airline#extensions#tabline#enabled = 1 ]])
+vim.cmd([[ let g:airline#extensions#tabline#left_sep = ' ' ]])
+vim.cmd([[ let g:airline#extensions#tabline#left_alt_sep = '|' ]])
+vim.cmd([[ let g:airline#extensions#tabline#formatter = 'unique_tail' ]])
+
